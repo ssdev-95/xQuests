249a6816cc178f40e29d93a55e89b74d5788ae92
xSallus <souza95salomao@gmail.com>
2021-06-09 11:27:11 -0400

    [UPDATE] Styles

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import { Toolbar, Button } from '@material-ui/core'
+
+function App() {
+  return (
+    <div>
+      <Toolbar>
+        <p>Welcome to the world of quests</p>
+        <Button style={{background:"blue",color:"red"}}>LOL</Button>
+      </Toolbar>
+    </div>
+  );
+}
+
+export default App;
