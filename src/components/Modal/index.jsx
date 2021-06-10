8b17e5b3bd4a99bd804ba95c7d179d1dcaba7be3
xSallus <souza95salomao@gmail.com>
2021-06-10 11:36:57 -0400

    [UPDATE] Working on modal styles

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -10,29 +10,59 @@ const useModal = makeStyles({
     height: "15rem",
     width: "15rem",
     borderRadius: "4px",
-    zIndex: "999",
+    zIndex: "600",
     margin: "auto",
-    background: "#f0f2f5",
+    background: "rgba(0,0,0,0)",
     display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
   icon: {
-      background: 'green'
-  }
+    filter: "invert(.5) sepia(.1) saturate(50) hue-rotate(90deg)",
+    height: "3.15rem",
+    width: "3.15rem",
+  },
+  container: {
+    outline: "none",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: 'relative'
+  },
+  input: {
+    height: "6rem",
+    width: "6rem",
+    fontSize: "4rem",
+    textAlign: "center",
+  },
+  submit: {
+    zIndex: 800,
+    background: "green",
+    color: "white",
+    position: 'absolute',
+    top: '12rem'
+  },
 });
 
 export const QuestModal = ({ isOpen }) => {
-  const { modal, icon } = useModal();
+  const { modal, icon, container, input, submit } = useModal();
 
   return (
     <Modal open={isOpen} className={modal}>
-      <div>
+      <div className={container}>
+        <Button>
+          <DoubleArrow
+            className={icon}
+            style={{ transform: "rotate(180deg)" }}
+          />
+        </Button>
+        <input className={input} max="99" type="number" />
         <Button>
           <DoubleArrow className={icon} />
         </Button>
-        <input type="number" />
-        <Button>
-          <DoubleArrow className={icon} />
-        </Button>
+      <Button className={submit}>Init</Button>
       </div>
     </Modal>
   );
