37bb4dbd8b355de29e3769e59f11647233c9d552
xSallus <souza95salomao@gmail.com>
2021-06-11 16:50:32 -0400

    [ADD] Finishing quiz context

---
+++ src/contexts/FinishedQuizContext.jsx
@@ -0,0 +1,86 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+import useQuestions from "./QuestionsContext";
+import useProfile from "./UserContext";
+
+const FinishedQuizContext = createContext({});
+
+export const FinishedQuizProvider = ({ children }) => {
+  const { user, updateUser } = useProfile();
+  const { quests } = useQuestions();
+
+  const [isFinishedQuizModalOpen, setIsFinishedQuizModalOpen] = useState(false)
+  const [points, setPoints] = useState(0);
+  const [quizResult, setQuizResult] = useState([]);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const toggleFisnishedQuizModal = () => {
+    setIsFinishedQuizModalOpen(!isFinishedQuizModalOpen)
+  }
+
+  const handleFinishedQuiz = (quizAnswers) => {
+    let finalPoints = 0;
+
+    quizAnswers.forEach((answer, index) => {
+      const correctAnswer = quests[index].answers[quests[index].correct_answer];
+      if (answer === correctAnswer) {
+        finalPoints += 10;
+      }
+      setQuizResult([
+        ...quizResult,
+        {
+          user_answer: answer,
+          status: answer === correctAnswer ? "correct" : "incorrect",
+        },
+      ]);
+    });
+
+    setPoints(finalPoints);
+    setHasFinished(true);
+    toggleFisnishedQuizModal()
+  };
+
+  /*analyzed-quiz = {
+      profile: User{},
+			questions:  [
+				{
+					user-answer: string,
+					status: string //either 'correct' or 'incorrect'
+        }
+      ]   
+  }*/
+
+  useEffect(() => {
+		updateUser({
+			...user,
+			points: points
+		})
+
+    if(hasFinished) {
+			localStorage.setItem("quiz", JSON.stringify({
+				profile: user,
+				questions: quizResult
+			}));
+
+			setHasFinished(false);
+		}
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasFinished]);
+
+  return (
+    <FinishedQuizContext.Provider
+      value={{
+        points,
+        isFinishedQuizModalOpen,
+        toggleFisnishedQuizModal,
+        handleFinishedQuiz
+      }}
+    >
+      {children}
+    </FinishedQuizContext.Provider>
+  );
+};
+
+export default function useFinishedQuiz() {
+  return useContext(FinishedQuizContext);
+}
